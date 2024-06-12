13e12a384ad268654512c627e2ae3cd9c483bea8
damipineda <damipinedacabrera@gmail.com>
2024-06-11 21:30:26 -0400

    Se agrega el codigo del challenge diario

---
+++ dia_1_challenge.py
@@ -0,0 +1,5 @@
+palabra_usuario = input('Ingresa una palabra o numero de 2 digitos diferentes: ') #Solicitamos al usuaio una palabra o numero de dos digitos diferentes.
+
+palabra_usuario_invertido = palabra_usuario[::-1] #Invertimos lo ingresado por el usuario
+
+print(f'La palabra inverida es:  {palabra_usuario_invertido}') #Iprimimos la palabra invertida
